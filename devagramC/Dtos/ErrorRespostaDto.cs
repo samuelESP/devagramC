dbecbd1d8309ee39bbc0544c0468939ade490a1a
Samuel Espíndola <62106502+samuelESP@users.noreply.github.com>
2025-04-13 18:44:12 -0300

    Realizando a implementação do login

---
+++ devagramC/Dtos/ErrorRespostaDto.cs
@@ -0,0 +1,9 @@
+﻿namespace devagramC.Dtos
+{
+    public class ErrorRespostaDto
+    {
+
+        public int Status { get; set; }
+        public string Descricao { get; set; }
+    }
+}
---
+++ devagramC/Dtos/LoginRequisicaoDto.cs
@@ -0,0 +1,8 @@
+﻿namespace devagramC.Dtos
+{
+    public class LoginRequisicaoDto
+    {
+        public string Email { get; set; }
+        public string Senha { get; set; }
+    }
+}
